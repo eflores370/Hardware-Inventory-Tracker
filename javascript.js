58414298b2c84d2fd9ea1d70f85139829cd509ee
eflores370 <emanuelflores@missiontechiess-MacBook-Air.local>
2017-03-01 14:18:20 -0800

    MTI 0.11

--- javascript.js
+++ javascript.js
@@ -3,14 +3,21 @@ var mainInventoryHTML = '';
 var mainInventoryDiv = $('.mainBody');
 var renderMainInventory = function (data) {
   data.records.forEach(function (item) {
+    if(item.fields['Serial Number / Asset Number']){
       mainInventoryHTML += '<h2>Serial Number/Asset Number: ' + item.fields['Serial Number / Asset Number'] + '</h2>';
-      mainInventoryHTML += '<p>Status: ' + item.fields['Status'] + '</p>';
-      mainInventoryHTML += '<p> Operating System: ' + item.fields['Operating System'] + '</p>';
-      mainInventoryHTML += '<p>Ram(GB): ' + item.fields['Ram GB'] + '</p>';
-      mainInventoryHTML += '<p>Storage(GB): ' + item.fields['Storage GB'] + '</p>';
+      if(item.fields['Desktop Model']) {
+        mainInventoryHTML += '<p>Desktop Model: ' + item.fields['Desktop Model'] + '</p>'; }
+      if(item.fields['Status']) {
+        mainInventoryHTML += '<p>Status: ' + item.fields['Status'] + '</p>';}
+      if(item.fields['Operating System']) {
+        mainInventoryHTML += '<p> Operating System: ' + item.fields['Operating System'] + '</p>';}
+      if(item.fields['Ram GB']) {
+        mainInventoryHTML += '<p>Ram(GB): ' + item.fields['Ram GB'] + '</p>';}
+      if(item.fields['Storage GB']) {
+        mainInventoryHTML += '<p>Storage(GB): ' + item.fields['Storage GB'] + '</p>';}
       mainInventoryHTML += '<hr />';
     }
-  );
+  });
   mainInventoryDiv.html(mainInventoryHTML);
 
 };
