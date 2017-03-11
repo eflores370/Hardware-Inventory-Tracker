b2fb20f67ec57f271f69eea939adb8b6ddfa15ae
eflores370 <emanuelflores@missiontechiess-MacBook-Air.local>
2017-03-11 15:21:01 -0800

    MTI 0.2

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Mission-Techies-Inventory
 Tracks the Mission Techies Inventory
+https://mission-techies.github.io/Mission-Techies-Inventory/
+Made by Emanuel Flores
---
+++ main.js
@@ -0,0 +1,46 @@
+var mainInventoryURL = 'https://api.airtable.com/v0/appztwEDDxgAVCwxF/Main%20Inventory?api_key=keykbC2FwErK6UFom';
+var mainInventoryHTML = '';
+var mainInventoryDiv = $('.mainBody');
+var renderMainInventory = function(data) {
+    data.records.forEach(function(item) {
+        if (item.fields['Serial Number / Asset Number']) {
+            mainInventoryHTML += "<div class='itemContainer'>"
+            mainInventoryHTML += "<div class='itemName'>"
+            mainInventoryHTML += '<h2>Serial Number/Asset Number: ' + item.fields['Serial Number / Asset Number'] + '<span class="caret"></span></h2>';
+            mainInventoryHTML += '</div>'
+            // Add Hide to class
+            mainInventoryHTML += "<div class='itemInfo hide'>"
+            if (item.fields['Desktop Model']) {
+                mainInventoryHTML += '<p>Desktop Model: ' + item.fields['Desktop Model'] + '</p>';
+            }
+            if (item.fields['Status']) {
+                mainInventoryHTML += '<p>Status: ' + item.fields['Status'] + '</p>';
+            }
+            if (item.fields['Operating System']) {
+                mainInventoryHTML += '<p> Operating System: ' + item.fields['Operating System'] + '</p>';
+            }
+            if (item.fields['Ram GB']) {
+                mainInventoryHTML += '<p>Ram: ' + item.fields['Ram GB'] + '</p>';
+            }
+            if (item.fields['Storage GB']) {
+                mainInventoryHTML += '<p>Storage: ' + item.fields['Storage GB'] + '</p>';
+            }
+            if (item.fields['CPU']) {
+                mainInventoryHTML += '<p>CPU: ' + item.fields['CPU'] + '</p>';
+            }
+            mainInventoryHTML += '</div>'
+            mainInventoryHTML += '</div>'
+            mainInventoryHTML += '<hr />';
+
+        }
+    });
+    mainInventoryDiv.html(mainInventoryHTML);
+
+};
+
+$.getJSON(mainInventoryURL, renderMainInventory);
+
+
+$(document).on('click', '.itemContainer', function() {
+    $(this).find('.itemInfo').toggleClass('hide');
+})
