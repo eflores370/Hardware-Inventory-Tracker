1a020c8eb6d832c83c1fd0c0ae4912597a03af95
Emanuel Flores <emanuelcflores@gmail.com>
2017-03-18 14:00:33 -0700

    MTS 0.2.11

--- additem.js
+++ additem.js
@@ -1,17 +1,29 @@
-// Version 0.2.10
+// Version 0.2.11
 var submitURL = 'https://api.airtable.com/v0/appztwEDDxgAVCwxF/Main%20Inventory?api_key=keykbC2FwErK6UFom';
 var form = $('#addItemForm');
+
+
+
+
 form.on('submit', function(e){
    e.preventDefault();
    var serialNumber = $(this).find('input[name=serialNumber]').val();
    var desktopModel = $(this).find('input[name=desktopModel]').val();
-   var vendor = $(this).find('input[name=vendor]').val();
-   var status = $(this).find('input[name=status]').val();
-   var operatingSystem = $(this).find('input[name=operatingSystem]').val();
+   var vendor = $(this).find('select[name=vendor]').val();
+   var status = $(this).find('select[name=status]').val();
+  //  var operatingSystem = $(this).find('input[name=operatingSystem]').val();
    var cpu = $(this).find('input[name=cpu]').val();
    var ram = $(this).find('input[name=ram]').val();
    var storage = $(this).find('input[name=storage]').val();
    var notes = $(this).find('textarea[name=notes]').val();
+  //  var ipAddress = '';
+
+    //Get IP Address
+    // $.getJSON("https://api.ipify.org?format=jsonp&callback=?",
+    //     function(json) {
+    //       ipAddress = (json.ip);
+    //       console.log(ipAddress);
+    //     });
 
    if (!serialNumber) {
       $(this).find('input[name=serialNumber]').addClass("error");
@@ -19,17 +31,19 @@ form.on('submit', function(e){
      return;
    }
 
+
    var data = {
      'fields': {
        'Serial Number / Asset Number': serialNumber,
        'Desktop Model': desktopModel,
        'Vendor': vendor,
        'Status': status,
-       'Operating System': operatingSystem,
+      //  'Operating System': operatingSystem,
        'CPU': cpu,
        'Ram GB': ram,
        'Storage GB': storage,
        'Notes': notes,
+      //  'IP Address': ipAddress
     }
    };
   $.post(submitURL, data, function(data){
